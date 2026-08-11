3b27d8e5f3fa336c443480bc515245f677f770e9
n1zen <114575802+n1zen@users.noreply.github.com>
2026-08-11 19:30:09 +0800

    added mining xp rewards for mining ores. made clearXp clear both levels and xp.

--- src/main/java/n1zen/eterniamod/EterniamodServer.java
+++ src/main/java/n1zen/eterniamod/EterniamodServer.java
@@ -8,12 +8,24 @@ import n1zen.eterniamod.commands.skills.admin.ClearXp;
 import n1zen.eterniamod.commands.skills.level.ShowAllSkillLevelSelf;
 import n1zen.eterniamod.commands.skills.level.ShowSpecificSkillLevelOther;
 import n1zen.eterniamod.commands.skills.level.ShowSpecificSkillLevelSelf;
+import n1zen.eterniamod.skills.PlayerSkillLevelState;
+import n1zen.eterniamod.skills.PlayerSkillXpState;
+import n1zen.eterniamod.skills.SkillType;
+import n1zen.eterniamod.skills.xp.rewards.BlockXpReward;
+import n1zen.eterniamod.skills.xp.rewards.BlockXpRewards;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 import static n1zen.eterniamod.Eterniamod.MOD_ID;
 
@@ -32,6 +44,9 @@ public class EterniamodServer implements DedicatedServerModInitializer {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> LOGGER.info("Eterniamod has been initialized!"));
 
         PlayerBlockBreakEvents.AFTER.register((level, player, blockPos, blockState, block) -> {
+            if (!level.isClientSide()) {
+                BlockBreakLvlUp((ServerLevel) level, player, blockState);
+            }
 
         });
 
@@ -56,6 +71,31 @@ public class EterniamodServer implements DedicatedServerModInitializer {
             ClearXp.register(dispatcher);
 
         });
+    }
+
+    private static void BlockBreakLvlUp(ServerLevel level, Player player, BlockState blockState) {
+        Block brokenBlock = blockState.getBlock();
+        BlockXpReward reward = BlockXpRewards.REWARDS.get(brokenBlock);
+
+        if (reward != null) {
+            double amount = reward.amount();
+            SkillType skillType = reward.skill();
+
+            UUID playerUUID = player.getUUID();
+
+            PlayerSkillLevelState lvlState = PlayerSkillLevelState.get(level);
+            PlayerSkillXpState xpState = PlayerSkillXpState.get(level);
+
+            xpState.addSkillExp(playerUUID, skillType, amount);
+
+            double xp = xpState.getSkillExp(playerUUID, skillType);
+
+            if (lvlState.levelledUp(xp, playerUUID,  skillType)) {
+                player.sendSystemMessage(
+                        Component.literal(skillType.name() + " levelled up!")
+                );
+            }
+        }
     }
 
 }
